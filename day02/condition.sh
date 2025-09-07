ae9255f42b086e1b1f582c5bacf773ddc6ab2b8d
Mohit Prajapati <mohitprajapati8963@gmail.com>
2025-09-07 09:36:19 +0000

    update

--- day02/condition.sh
+++ day02/condition.sh
@@ -1,15 +1,21 @@
 #!/bin/bash 
 
-
+# input from user with prompt
 read -p "enter the girl: " girl
-read -p "boy love % " love 
+read -p "boy love % " love
 
+
+# If condition 
 if [[ $girl == "radha" ]];
 then 
 	echo "boy is loyal"
+
+# elif condition 
 elif [[$love -ge 100 ]];
 then
 	echo "boy is loyal"
+
+# else condition 
 else
         echo "boy is not loyal"
 fi 
