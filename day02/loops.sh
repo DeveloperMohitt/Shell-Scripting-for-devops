e182956431ffc95dfc6cdf937914fccaf85839c1
Mohit Prajapati <mohitprajapati8963@gmail.com>
2025-09-07 10:13:57 +0000

    loops updated

---
+++ day02/loops.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# This is for  
+
+<< task
+
+for ((i=1 ; i<=5; i++));
+do
+	mkdir "demo$i"
+done
+task
+
+# This is while loop 
+
+num=0
+
+while [[ $num -le 10 ]]
+do
+	echo $num
+	num=$((num+2))
+done
